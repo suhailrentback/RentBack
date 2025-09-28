686ae53fa9cae134d8fa3546b636c495288ffbfa
suhailrentback <suhail@rentback.app>
2025-09-29 01:08:08 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 // middleware.ts
-export { auth as middleware } from "@/app/api/auth/[...nextauth]/route";
+export { auth as middleware } from "./app/api/auth/[...nextauth]/route";
 
 export const config = {
   matcher: ["/tenant/:path*", "/landlord/:path*", "/admin/:path*"],
