08c22e3039bee404297a7a6eb91345b02f36641d
suhailrentback <suhail@rentback.app>
2025-09-28 20:59:37 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { typedRoutes: true, optimizePackageImports: ['react'] }
+  experimental: {
+    typedRoutes: false, // turn off so string hrefs are fine
+  },
 };
+
 module.exports = nextConfig;
