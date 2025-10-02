694dc137524434249f0bf6c253b03ff78c57868d
suhailrentback <suhail@rentback.app>
2025-10-02 22:03:01 +0200

    Update useLang.ts

--- hooks/useLang.ts
+++ hooks/useLang.ts
@@ -3,10 +3,6 @@
 import { useContext } from "react";
 import { LangContext } from "@/lib/i18n";
 
-/**
- * Custom hook to access current language (en/ur).
- */
 export function useLang() {
-  const { lang, setLang } = useContext(LangContext);
-  return { lang, setLang };
+  return useContext(LangContext); // { lang, setLang }
 }
