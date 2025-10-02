12c5f6646510f77b37e650d95658a9116660d663
suhailrentback <suhail@rentback.app>
2025-10-02 17:19:40 +0200

    Update RoleNavTenant.tsx

--- components/nav/RoleNavTenant.tsx
+++ components/nav/RoleNavTenant.tsx
@@ -1,3 +1,4 @@
+// components/nav/RoleNavTenant.tsx
 "use client";
 
 import Link from "next/link";
@@ -64,9 +65,4 @@ function ProfileIcon({ active }: { active?: boolean }) {
       <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8V22h19.2v-2.8c0-3.2-6.4-4.8-9.6-4.8z" />
     </svg>
   );
-}
-
-// tiny util colocated to avoid a shared utils import for now
-function cn(...classes: (string | false | null | undefined)[]) {
-  return classes.filter(Boolean).join(" ");
 }
